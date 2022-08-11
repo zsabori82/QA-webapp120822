2f2f5b8b3f053a68f128a98f0c5764782ecb566c
Yazah <Yazah@LAPTOP-PA9GU9IN>
2022-08-12 02:06:23 +0430

    Add project files.

---
+++ QA-webapp120822/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QA_webapp120822.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
